0e22ea29ec3eb02990fc0b9322e0c96f70ea1ce2
Daniel Queijeiro <dany.queijeiro@hotmail.com>
2026-06-12 14:19:03 -0600

    prueba

--- .notebook_tmp_inputs/similar_pair_a.py
+++ .notebook_tmp_inputs/similar_pair_a.py
@@ -1,16 +1,8 @@
-H = int(input())
+def fibonacci(n):
+    x = 0
+    y = 1
+    while n > 0:
+        print(x, end=" ")
+        x, y = y, x + y
 
-def attack(h):
-    if h == 1:
-        return 0
-    r = 2
-    h //= 2
-    ans = 2
-    while h > 1:
-        h //= 2
-        r *= 2
-        ans += r
-    return ans
-    
-    
-print(1 + attack(H))
+print(fibonacci(10))
--- .notebook_tmp_inputs/similar_pair_b.py
+++ .notebook_tmp_inputs/similar_pair_b.py
@@ -1,13 +1,9 @@
-h = int(input())
+def fibonacci(n):
+    a, b = 0, 1
+    seq = []
+    for _ in range(n):
+        seq.append(a)
+        a, b = b, a + b
+    return seq[:n]
 
-ans = 0
-n_enemy = 1
-while h > 1:
-    h //= 2
-    ans += n_enemy
-    n_enemy *= 2
-
-if h == 1:
-    ans += n_enemy
-
-print(ans)
+print(fibonacci(10))
